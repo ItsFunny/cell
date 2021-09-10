c0b47c583db2bef672a642d31a637e0ab4fe9620
lvcong <lvcong@ebidsun.com>
2021-09-11 00:47:35 +0800

    fuck spring version match

--- cell-gate/cell-http-gate/src/main/java/com/cell/config/GatewayConfiguration.java
+++ cell-gate/cell-http-gate/src/main/java/com/cell/config/GatewayConfiguration.java
@@ -4,7 +4,8 @@ import com.cell.annotations.ActiveConfiguration;
 import com.cell.annotations.Plugin;
 import com.cell.lb.DefaultWeightRoubineStrategy;
 import com.cell.lb.ILoadBalancerStrategy;
-import org.springframework.cloud.gateway.filter.factory.AddRequestHeaderGatewayFilterFactory;
+import org.springframework.cloud.gateway.filter.factory.*;
+import org.springframework.cloud.gateway.handler.predicate.HostRoutePredicateFactory;
 import org.springframework.cloud.gateway.handler.predicate.PathRoutePredicateFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
@@ -47,15 +48,117 @@ public class GatewayConfiguration
         return new PathRoutePredicateFactory();
     }
 
+    @Bean
+    public HostRoutePredicateFactory hostRoutePredicateFactory()
+    {
+        return new HostRoutePredicateFactory();
+    }
+
+    @Bean
+    public RewritePathGatewayFilterFactory rewritePathGatewayFilterFactory()
+    {
+        return new RewritePathGatewayFilterFactory();
+    }
+
+    @Bean
+    public RetryGatewayFilterFactory retryGatewayFilterFactory()
+    {
+        return new RetryGatewayFilterFactory();
+    }
+
+    @Bean
+    public SetPathGatewayFilterFactory setPathGatewayFilterFactory()
+    {
+        return new SetPathGatewayFilterFactory();
+    }
+
+
+    @Bean
+    public SetRequestHeaderGatewayFilterFactory setRequestHeaderGatewayFilterFactory()
+    {
+        return new SetRequestHeaderGatewayFilterFactory();
+    }
+
+    @Bean
+    public SetResponseHeaderGatewayFilterFactory setResponseHeaderGatewayFilterFactory()
+    {
+        return new SetResponseHeaderGatewayFilterFactory();
+    }
+
+    @Bean
+    public RewriteResponseHeaderGatewayFilterFactory rewriteResponseHeaderGatewayFilterFactory()
+    {
+        return new RewriteResponseHeaderGatewayFilterFactory();
+    }
+
+    @Bean
+    public RewriteLocationResponseHeaderGatewayFilterFactory rewriteLocationResponseHeaderGatewayFilterFactory()
+    {
+        return new RewriteLocationResponseHeaderGatewayFilterFactory();
+    }
+
+    @Bean
+    public SetStatusGatewayFilterFactory setStatusGatewayFilterFactory()
+    {
+        return new SetStatusGatewayFilterFactory();
+    }
+
+    @Bean
+    public SaveSessionGatewayFilterFactory saveSessionGatewayFilterFactory()
+    {
+        return new SaveSessionGatewayFilterFactory();
+    }
+
+    @Bean
+    public StripPrefixGatewayFilterFactory stripPrefixGatewayFilterFactory()
+    {
+        return new StripPrefixGatewayFilterFactory();
+    }
+
+    @Bean
+    public RequestHeaderToRequestUriGatewayFilterFactory requestHeaderToRequestUriGatewayFilterFactory()
+    {
+        return new RequestHeaderToRequestUriGatewayFilterFactory();
+    }
+
+    @Bean
+    public RequestSizeGatewayFilterFactory requestSizeGatewayFilterFactory()
+    {
+        return new RequestSizeGatewayFilterFactory();
+    }
+
+    @Bean
+    public RequestHeaderSizeGatewayFilterFactory requestHeaderSizeGatewayFilterFactory()
+    {
+        return new RequestHeaderSizeGatewayFilterFactory();
+    }
 
     @Plugin
     public RouteLocator myRoutes(RouteLocatorBuilder builder)
     {
+//        RouteLocatorBuilder.Builder routes = builder.routes();
+//        routes.route(p -> p
+//                .path("/get")
+//                .filters(f -> f.addRequestHeader("Hello", "World"))
+//                .uri("http://www.baidu.com:80")).build();
+//        return routes.build();
         return builder.routes()
-                .route(p -> p
-                        .path("/get")
-                        .filters(f -> f.addRequestHeader("Hello", "World"))
-                        .uri("http://www.baidu.com:80"))
+                .route("path_route", r -> r.path("/get")
+                        .uri("http://httpbin.org"))
+                .route("host_route", r -> r.host("*.myhost.org")
+                        .uri("http://httpbin.org"))
+                .route("rewrite_route", r -> r.host("*.rewrite.org")
+                        .filters(f -> f.rewritePath("/foo/(?<segment>.*)", "/${segment}"))
+                        .uri("http://httpbin.org"))
+                .route("hystrix_route", r -> r.host("*.hystrix.org")
+//                        .filters(f -> f.hystrix(c -> c.setName("slowcmd")))
+                        .uri("http://httpbin.org"))
+                .route("hystrix_fallback_route", r -> r.host("*.hystrixfallback.org")
+//                        .filters(f -> f.hystrix(c -> c.setName("slowcmd").setFallbackUri("forward:/hystrixfallback")))
+                        .uri("http://httpbin.org"))
+                .route("limit_route", r -> r
+                        .host("*.limited.org").and().path("/anything/**")
+                        .uri("http://httpbin.org"))
                 .build();
     }
 }
