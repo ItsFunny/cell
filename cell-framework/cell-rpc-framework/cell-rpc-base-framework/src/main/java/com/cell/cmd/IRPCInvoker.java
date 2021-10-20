0f3eea42afe024b2e156095281769ee8a7070f4d
lvcong <lvcong@ebidsun.com>
2021-10-20 22:59:20 +0800

    rpc 添加基础模型

---
+++ cell-framework/cell-rpc-framework/cell-rpc-base-framework/src/main/java/com/cell/cmd/IRPCInvoker.java
@@ -0,0 +1,14 @@
+package com.cell.cmd;
+
+/**
+ * @author Charlie
+ * @When
+ * @Description
+ * @Detail
+ * @Attention:
+ * @Date 创建时间：2021-10-20 22:08
+ */
+public interface IRPCInvoker
+{
+
+}
---
+++ cell-framework/cell-rpc-framework/cell-rpc-base-framework/src/main/java/com/cell/cmd/server/abs/AbstractBaseRPCServerInvoker.java
@@ -0,0 +1,21 @@
+package com.cell.cmd.server.abs;
+
+import com.cell.cmd.server.IRPCServerInvoker;
+import com.cell.context.IRPCContext;
+
+/**
+ * @author Charlie
+ * @When
+ * @Description
+ * @Detail
+ * @Attention:
+ * @Date 创建时间：2021-10-20 22:03
+ */
+public abstract class AbstractBaseRPCServerInvoker implements IRPCServerInvoker
+{
+    @Override
+    public void execute(IRPCContext context)
+    {
+
+    }
+}
