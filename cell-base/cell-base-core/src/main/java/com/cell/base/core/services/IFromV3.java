baabb8662f53c5d4fe77d27ac7ca01469d207e52
itsfunny <18757883747@163.com>
2022-09-15 09:04:41 +0800

    add fromv3

---
+++ cell-base/cell-base-core/src/main/java/com/cell/base/core/services/IFromV3.java
@@ -0,0 +1,33 @@
+package com.cell.base.core.services;
+
+import com.cell.base.common.consumers.IConsumer;
+import com.cell.base.common.enums.ErrorEnums;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface IFromV3<F, T>
+{
+    T onFrom(F f);
+
+    default List<T> from(List<F> f, IConsumer<T, ErrorEnums> consumer)
+    {
+        List<T> ret = new ArrayList<>();
+        for (F f1 : f)
+        {
+            T t = this.onFrom(f1);
+            if (null != consumer)
+            {
+                ErrorEnums consume = consumer.consume(t);
+                if (consume == ErrorEnums.BREAK)
+                {
+                    break;
+                }else if (consume==ErrorEnums.CONTINUE){
+                    continue;
+                }
+            }
+            ret.add(t);
+        }
+        return ret;
+    }
+}
